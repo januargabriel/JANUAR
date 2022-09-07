50132a0d1589dc125f14b01b678e7b60145578a7
januargabriel <108562500+januargabriel@users.noreply.github.com>
2022-09-07 09:36:21 +0700

    Update form_jurusan.php

--- admin/form_jurusan.php
+++ admin/form_jurusan.php
@@ -274,26 +274,26 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/jquery/jquery.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/jquery/jquery.min.js'?>"></script>
 <!-- Bootstrap 4 -->
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
 <!-- DataTables  & Plugins -->
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/datatables/jquery.dataTables.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/lugins/datatables-responsive/js/responsive.bootstrap4.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/dataTables.buttons.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/jszip/jszip.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/pdfmake/pdfmake.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/pdfmake/vfs_fonts.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/buttons.html5.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/buttons.print.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/buttons.colVis.min.js'?>"></script>
-<script src="<?= 'http://file.smkbatikpwrj.sch.id/assets/dist/js/adminlte.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/datatables/jquery.dataTables.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/dataTables.buttons.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/jszip/jszip.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/pdfmake/pdfmake.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/pdfmake/vfs_fonts.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/buttons.html5.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/buttons.print.min.js'?>"></script>
+<script src="<?='https://file.smkbatikpwrj.sch.id/assets/plugins/datatables-buttons/js/buttons.colVis.min.js'?>"></script>
+<!-- AdminLTE App -->
+<script src="<?='https://file.smkbatikpwrj.sch.id/assetst/dist/js/adminlte.min.js'?>"></script>
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 <script>
-
   $(function () {
     $("#example1").DataTable({
       "responsive": true, "lengthChange": false, "autoWidth": false,
